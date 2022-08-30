4dd0ffe3426c0f2dd79a7504c32a40b0de041189
arominddo <godhsb@naver.com>
2022-08-30 14:07:02 +0900

    2022.08.30.(2)

---
+++ src/Bronze/Bronze5/No_10718_We_love_kriii.java
@@ -0,0 +1,11 @@
+package Bronze.Bronze5;
+
+public class No_10718_We_love_kriii {
+
+	public static void main(String[] args) {
+		System.out.println("강한친구 대한육군");
+		System.out.print("강한친구 대한육군");
+
+	}
+
+}
---
+++ src/Bronze/Bronze5/No_2557_HelloWorld.java
@@ -0,0 +1,10 @@
+package Bronze.Bronze5;
+
+public class No_2557_HelloWorld { 								//백준 제출시에 Main으로 변경
+
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+
+	}
+
+}
